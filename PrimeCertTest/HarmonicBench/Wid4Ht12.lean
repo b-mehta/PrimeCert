f717894100529c7972fdca337129547c42ebad73
Bhavik Mehta <bhavikmehta8@gmail.com>
2026-09-19 00:10:21 +0000

    measure the cost of four interval lengths sieved by the same primes

---
+++ PrimeCertTest/HarmonicBench/Wid4Ht12.lean
@@ -0,0 +1,20 @@
+/-
+Copyright (c) 2026 Bhavik Mehta. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Bhavik Mehta
+-/
+module
+
+public meta import PrimeCert.Meta.PrimeHarmonic
+public meta import PrimeCert.SegmentedSieve
+public import PrimeCertTest.HarmonicBench.SieveHt12
+
+/-! # Interval length 3145728 from `10 ^ 12`, sieved by the primes up to 1000007 (see `Wid1Ht12`) -/
+
+namespace PrimeCert.Wid4Ht12
+
+set_option maxRecDepth 4000000 in
+set_option Elab.async false in
+run_harmonic_series 1000000000001 1048576 1000007 20 2048 1536 1 1
+
+end PrimeCert.Wid4Ht12
